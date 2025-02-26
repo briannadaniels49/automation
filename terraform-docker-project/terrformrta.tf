986d803d2cf995dc91465dd6fc9e121d53b15fc1
Emanuel Burgess <emanuelburgess@Mac.attlocal.net>
2025-02-25 20:57:31 -0500

    update

--- terraform-docker-project/terrformrta.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "a01" {
-  subnet_id      = aws_subnet.pub1.id
-  route_table_id = aws_route_table.routetable1.id
-}
-
-resource "aws_route_table_association" "a02" {
-  subnet_id      = aws_subnet.pub2.id
-  route_table_id = aws_route_table.routetable2.id
-}
